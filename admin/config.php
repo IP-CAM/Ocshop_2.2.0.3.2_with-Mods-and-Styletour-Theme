9303102c161136507cd80f13b37d952dace47db1
Konstantin Semenov <semenov.k.u@gmail.com>
2017-06-07 19:20:02 +0300

    Свежеустановленный ocStore

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop.konstantinsemenov.com/admin/');
+define('HTTP_CATALOG', 'http://shop.konstantinsemenov.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop.konstantinsemenov.com/admin/');
+define('HTTPS_CATALOG', 'http://shop.konstantinsemenov.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/admin/');
+define('DIR_SYSTEM', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/');
+define('DIR_IMAGE', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/image/');
+define('DIR_LANGUAGE', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/config/');
+define('DIR_CACHE', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'semenoh3_shop');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'semenoh3_shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop.konstantinsemenov.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop.konstantinsemenov.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/catalog/');
+define('DIR_SYSTEM', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/');
+define('DIR_IMAGE', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/image/');
+define('DIR_LANGUAGE', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/config/');
+define('DIR_CACHE', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/s/semenoh3/shop.konstantinsemenov.com/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'semenoh3_shop');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'semenoh3_shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
